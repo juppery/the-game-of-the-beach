d7a74764ef138d46b146937c357eda39c90ab10e
WonkaChild <william.przybyl@gmail.com>
2023-07-10 19:49:07 -0400

    Camera movement script

--- scripts/Camera Flipper Script.gd
+++ scripts/Camera Flipper Script.gd
@@ -4,8 +4,13 @@ extends Area2D
 
 
 func _ready():
+	pass
+#iowubgiuwbgriwerguiwnegfoiwergiubewgiubgeu
+
+func _process(delta):
+	pass
+# AEFIENIUEWFIEWUFN
+
+
+func _on_Camera_Flipper_mouse_entered():
 	pass # Replace with function body.
-
-
-#func _process(delta):
-#	pass
