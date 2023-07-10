677c6cdbd927fe3a395345994780a654645f2a99
juppery <josiahbirt.13@gmail.com>
2023-07-09 21:22:51 -0400

    sliding window ACTUALLY works and i feel so good
    this is all gonna be destroyed probably once we actually get the pictures and i might be a bit sad. but at least i gopt this to work and i know something new

--- scripts/windowclosed.gd
+++ scripts/windowclosed.gd
@@ -3,8 +3,10 @@ extends Sprite
 #makes it so i can refer to this sprite as its name
 onready var window_closed_sprite = get_node(".")
 
-var windowDown = false
-var windowButtonUp = false
+
+
+
+var windowOpen = true
 #makes a variable that i can change and then later convert to the position of the sprite
 var velocity = Vector2.ZERO
 
@@ -14,32 +16,30 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#calculate the new position without modifying the sprite's position directly
-	var new_pos = position
-	#makes it so the sprite can't go above or below those numbers
+func _process(delta):	
 	
-	if windowDown == true and position.y < 238:
+	
+	#refer to line 7, or whichever talsk about 238
+	if windowOpen == true and position.y != 0:
+		velocity.y -= 50*delta
+	
+	if windowOpen != true and position.y != 238:
+		velocity.y += 50*delta
 		
-		velocity.y -= 90*delta
-		new_pos.y += velocity.y + 28
-		
-	if windowDown == false and position.y > 0:
-		velocity.y += 90*delta
-		new_pos.y -= velocity.y + 28
-	
-	#everything here calculate the new position without modifying the sprite's actual position directly. basically giving it one instruction instead of a billion i think
-	new_pos.y = clamp(new_pos.y, 0, 238)
-	position.y = new_pos.y
-	
-	if position.y >= 238 and windowButtonUp:
-		windowDown = false
-
-
-
+	if position.y == 0 and velocity.y < 0:
+		velocity.y = 0
+	if position.y == 238 and velocity.y > 0:
+		velocity.y = 0
+	position.y += velocity.y
+	position.y = clamp(position.y, 0, 238)
+	print("velocity ", velocity.y) #debug purposes
+	print("position ", position.y) #debug purposes
 func _on_close_window_button_button_down():
-	windowDown = true
-	windowButtonUp = false
+	windowOpen = false
 
 func _on_close_window_button_button_up():
-	windowButtonUp = true
+	windowOpen = true
+
+
+func _on_close_window_button_mouse_exited():
+	windowOpen = true
