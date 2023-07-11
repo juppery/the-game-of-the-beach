3ed7121cd603e97d85f2d15bd61471638c4bd0b7
juppery <josiahbirt.13@gmail.com>
2023-07-10 21:36:58 -0400

    added some organizarion that is CHANGABLE
    glog

--- scripts/Camera pan.gd
+++ scripts/Camera pan.gd
@@ -1,16 +1,17 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var gazebo = get_node("PLACEHOLDER IMAGES AHAHHAHH/gazebo")
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _process(delta):
+	var leftedgeofgazebo = gazebo.position.x
+	print(leftedgeofgazebo)
+	if leftedgeofgazebo < 1920:
+		gazebo.position.x += 10
+
