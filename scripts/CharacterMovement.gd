83ff0c70891744ac8e6a392ed3461bc3f9b48618
juppery <josiahbirt.13@gmail.com>
2023-07-14 16:31:21 -0400

    the code now works. ben and danny move

--- scripts/CharacterMovement.gd
+++ scripts/CharacterMovement.gd
@@ -1,98 +1,105 @@
 extends Node2D
 
 onready var windowscript = get_node("noahpanningview/window_closed_sprite")
-onready var benLocation = "schoolroom"
-onready var dannyLocation = "schoolroom"
-onready var hour = 1
+#onready var doorscript = getnode (node with the doorscript)
 
-func _process(delta):
-	print("danny's location is "+dannyLocation)
-	print("ben's location is "+benLocation)
+onready var benLocation = "schoolroom"
+onready var benAgressionValue = 20
+
+onready var dannyLocation = "schoolroom"
+onready var dannyAgressionValue = 20
+
+onready var HourLabel = get_node("HourLabel")
+onready var hour = 0
+
+#debug purposes, this is for
+#func _process(delta):
+#	print("danny loc: "+dannyLocation)
+#	print("ben loc: "+benLocation)
 
 
 
 
 func _on_HourTimer_timeout():
-	pass # Replace with function body.
-
-
-
+	if hour == 0:
+		HourLabel.text = "1 AM"
+	if hour == 1:
+		HourLabel.text = "2 AM"
+	if hour == 2:
+		HourLabel.text = "3 AM"
+	if hour == 3:
+		HourLabel.text = "4 AM"
+	if hour == 4:
+		HourLabel.text = "5 AM"
+	if hour == 5:
+		pass #PLAYER WINS!!! WOOOOOOOOOO!!!!!!! maybe figure out a way to save the nights won?
 
 
 
 
 func _on_BenTimer_timeout():
-	check hour
-	check current agression number
-	if hour == 1:
-		roll number 1-20
-		if [rolled number] <= [agression number]:
-			if benLocation(i''ll make that a variable) == "schoolroom":
+	
+	var twentySidedDie = rand_range(1, 20)
+	var hundredSidedDie = rand_range(1, 100)
+	if twentySidedDie <= benAgressionValue:
+		if benLocation == "schoolroom":
 			benLocation = "backroom"
-			elif benLocation == "backroom":
-				roll number 1-100
-				if [rolled number] <= 35:
-					benLocation = "schoolroom"
-				elif [rolled number] > 35:
-					benLocation = "kitchen"
-			elif benLocation == "kitchen":
-				roll number 1-100
-				if [rolled number] <= 25:
-					benLocation = "samsroom"
-				elif [rolled number] > 25 and < 50:
-					benLocation = "schoolroom"
-				elif [rolled number] >= 50:
-					benLocation = "stairs" #referring to the bottom of the stairs
-			elif benLocation == "samsroom":
+		elif benLocation == "backroom":
+			if hundredSidedDie <= 35:
+				benLocation = "schoolroom"
+			elif hundredSidedDie > 35:
 				benLocation = "kitchen"
-			elif benLocation == "stairs":
-				roll number 1-100
-				if [rolled number] <= 40:
-					benLocation = "kitchen"
-				elif [rolled number] > 40 and <= 90:
-					benLocation = "bensroom"
-				elif [rolled number] > 90:
-					benLocation = "door"
-			elif benLocation == "door":
-				if doorscript.doorOpen == true:  #i haven't made the door script or the door variable yet. that'll happen
-					print("you lose stoobid")
-					#signal JUMPSCARE PLAYER !!!! AHHHH!!! SCARY!! #play a gif and over the game
-				elif doorscript.doorOpen == false:
-					benLocation = "schoolroom" #we can change this
+		elif benLocation == "kitchen":
+			if hundredSidedDie <= 25:
+				benLocation = "samsroom"
+			elif hundredSidedDie > 25 and hundredSidedDie < 50:
+				benLocation = "backroom"
+			elif hundredSidedDie >= 50:
+				benLocation = "stairs" #referring to the bottom of the stairs
+		elif benLocation == "samsroom":
+			benLocation = "kitchen"
+		elif benLocation == "stairs":
+			if hundredSidedDie <= 40:
+				benLocation = "kitchen"
+			elif hundredSidedDie > 40 and hundredSidedDie <= 90:
+				benLocation = "bensroom"
+			elif hundredSidedDie > 90:
+				benLocation = "door"
+#		elif benLocation == "door":
+#			if doorscript.doorOpen == true:  #i haven't made the door script or the door variable yet. that'll happen
+#				print("you lose stoobid")
+#				#signal JUMPSCARE PLAYER !!!! AHHHH!!! SCARY!! #play a gif and over the game
+#			elif doorscript.doorOpen == false:
+#				benLocation = "schoolroom" #we can change this
+	print("ben loc: "+benLocation)
 
-			
-			
-			
-			
-			
-			
 
 
 func _on_DannyTimer_timeout():
-	check hour
-	check current agression number
-	if hour == 1:
-		roll number 1-20
-		if [rolled number] <= [agression number]:
-			if dannyLocation != "dannysroom":
-				roll number 1-100
-				if [rolled number] < 15:
-					dannyLocation = "schoolroom"
-				elif [rolled number] >= 15 and < 30:
-					dannyLocation = "backroom"
-				elif [rolled number] >= 30 and < 40:
-					dannyLocation = "kitchen"
-				elif [rolled number] >= 40 and < 55:
-					dannyLocation = "samsroom"
-				elif [rolled number] >= 55 and < 80:
-					dannyLocation = "stairs"
-				elif [rolled number] >= 80:
-					dannyLocation = "dannysroom"
-			elif dannyLocation == "dannysroom":
-				if doorscript.doorOpen == true:  #i haven't made the door script or the door variable yet. that'll happen
-					signal JUMPSCARE PLAYER !!!! AHHHH!!! SCARY!! #play a gif and over the game
-				elif doorscript.doorOpen == false:
-					dannyLocation = "kitchen" #we can change this
+	
+	var twentySidedDie = rand_range(1, 20)
+	var hundredSidedDie = rand_range(1, 100)
+	if twentySidedDie <= dannyAgressionValue:
+		if dannyLocation != "dannysroom":
+			if hundredSidedDie < 15:
+				dannyLocation = "schoolroom"
+			elif hundredSidedDie >= 15 and hundredSidedDie < 30:
+				dannyLocation = "backroom"
+			elif hundredSidedDie >= 30 and hundredSidedDie < 40:
+				dannyLocation = "kitchen"
+			elif hundredSidedDie >= 40 and hundredSidedDie < 55:
+				dannyLocation = "samsroom"
+			elif hundredSidedDie >= 55 and hundredSidedDie < 80:
+				dannyLocation = "stairs"
+			elif hundredSidedDie >= 80:
+				dannyLocation = "dannysroom"
+#		elif dannyLocation == "dannysroom":
+#			if doorscript.doorOpen == true:  #i haven't made the door script or the door variable yet. that'll happen
+#				signal JUMPSCARE PLAYER !!!! AHHHH!!! SCARY!! #play a gif and over the game
+#			elif doorscript.doorOpen == false:
+#				dannyLocation = "kitchen" #we can change this
+	print("danny loc: "+dannyLocation)
+
 
 
 
