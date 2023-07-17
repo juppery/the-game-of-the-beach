47f9e3c96dd0a90899aee6ec12c68bef01d8e68a
juppery <josiahbirt.13@gmail.com>
2023-07-17 19:05:11 -0400

    added flippability

--- scripts/Camera Flipper Script.gd
+++ scripts/Camera Flipper Script.gd
@@ -1,15 +1,26 @@
 extends Node2D
 
+onready var flipgif = get_node("Camera Flipper/CameraFlip")
+onready var uiimages = get_node("UI Images")
+onready var cameraimages = get_node("Camera Images")
 
+onready var camerasactive = false
 
 
 func _ready():
-	pass
-
+	uiimages.visible = false
+	cameraimages.visible = false
 
 func _process(delta):
 	pass
 
 
 func _on_Camera_Flipper_mouse_entered():
-	print("big piner")
+	flipgif.play()
+
+
+
+func _on_CameraFlip_animation_finished():
+	flipgif.visible = false
+	uiimages.visible = true
+	cameraimages.visible = true
--- scripts/CameraImageBackerflicker.gd
+++ scripts/CameraImageBackerflicker.gd
@@ -5,7 +5,7 @@ onready var backer = get_node(".")
 func _process(delta):
 	var preglorg = rand_range(1,8) #edit this to make the border flash more often/less often
 	if preglorg < 2:
-		var glorg = rand_range(1, 1.1) # edit this to edit the range of shades of white/black
+		var glorg = rand_range(1, 1.2) # edit this to edit the range of shades of white/black
 		backer.color.r = 1/glorg
 		backer.color.g = 1/glorg
 		backer.color.b = 1/glorg
