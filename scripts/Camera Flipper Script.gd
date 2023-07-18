a1185945014338e35192f989500a4dac1ec99d11
juppery <josiahbirt.13@gmail.com>
2023-07-18 04:25:51 -0400

    added fake code

--- scripts/Camera Flipper Script.gd
+++ scripts/Camera Flipper Script.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-onready var flipactivator = get_node("Camera Flipper/FlipActivator")
-onready var reverseflipgif = get_node("Camera Flipper/ReverseCameraFlip")
-onready var flipgif = get_node("Camera Flipper/CameraFlip")
 onready var uiimages = get_node("UI Images")
 onready var cameraimages = get_node("Camera Images")
-onready var flipicon = get_node("Camera Flipper/CollisionShape2D/FlipIcon")
+
+onready var flipactivator = get_node("Camera Activation/Camera Flipper/FlipActivator")
+onready var reverseflipgif = get_node("Camera Activation/ReverseCameraFlip")
+onready var flipgif = get_node("Camera Activation/CameraFlip")
+onready var flipicon = get_node("Camera Activation/FlipIcon")
 
 
 
@@ -14,35 +15,29 @@ onready var camerasActive = false
 onready var mouseInActivator = false
 
 func _ready():
-	uiimages.visible = false
-	cameraimages.visible = false
+	pass
+	#make everything invisible but the FlipActivator
 	
-	flipgif.visible = false
-	reverseflipgif.visible = false
-func _process(delta):
-	if camerasActive == true:
-		uiimages.visible = true
-		cameraimages.visible = true
-		
-	if mouseInActivator == false and flipgif.visible == false and reverseflipgif.visible == false:
-		flipactivator.visible = true
-	else:
-		flipactivator.visible = false
-		
 func _on_Camera_Flipper_mouse_entered():
-	mouseInActivator = true
-	
-	if camerasActive == false:
-		flipgif.visible = true
-		flipgif.play()
-	
-	if camerasActive == true:
-		reverseflipgif.play()
-		camerasActive = false
+	pass
+	#if the cameras aren't active:
+		#deactivate the FlipActivator(temporarily)
+		#make the flip gif visible
+		#play the camera flip gif
+	#if the cameras are active:
+		#deactivate the flipactivator
+		#make the reverseflipgif visible
+		#play the reverse camera flip gif
 func _on_Camera_Flipper_mouse_exited():
-	mouseInActivator = false
-
+	pass
+	#don't need naything yet
 func _on_CameraFlip_animation_finished():
-	camerasActive = true
-	flipgif.visible = false
-	
+	pass
+	#make the flipgif invisible
+	#show the rest of the ui
+	#the cameras are now active
+
+
+func _on_ReverseCameraFlip_animation_finished():
+	pass
+	#make the reverseflipgif invisible
