536997e1affdf62114da68d419f0ad34a987828e
WonkaChild <william.przybyl@gmail.com>
2023-08-09 19:26:45 -0400

    Menu screen stuff once again

--- scenes/Menu.gd
+++ scenes/Menu.gd
@@ -11,6 +11,5 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_PlayButton_pressed():
+	pass
