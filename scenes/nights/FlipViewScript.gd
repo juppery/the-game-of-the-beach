d49864bcdb790fe955181940339557061b3aa1f0
juppery <josiahbirt.13@gmail.com>
2023-08-14 19:58:01 -0400

    added a gif that doesn't work

--- scenes/nights/FlipViewScript.gd
+++ scenes/nights/FlipViewScript.gd
@@ -5,3 +5,11 @@ extends Node2D
 
 func _ready():
 	pass
+
+
+func _on_LeftFlipBox_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_RightFlipBox_area_entered(area):
+	pass # Replace with function body.
