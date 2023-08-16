c68768f43e21142ee49c86c193a7d5a1ce0502ad
juppery <josiahbirt.13@gmail.com>
2023-08-16 18:59:16 -0400

    Revert "Revert "Merge branch 'main' of https://github.com/juppery/the-game-of-the-beach""
    This reverts commit 678de2f7ce7185c5a195971a8e2a5148600cecb7.

--- scenes/nights/FlipViewScript.gd
+++ scenes/nights/FlipViewScript.gd
@@ -1,7 +1,22 @@
 extends Node2D
-
-
+onready var DoorNode = get_node("Door")
+onready var ShowerNode = get_node("Shower")
+onready var WindowNode = get_node("Window")
+onready var GifNode = get_node("ViewControl/Gifs")
+onready var ShowerToWindowGif = get_node("ViewControl/Gifs/showertowindow")
 
 
 func _ready():
-	pass
+	DoorNode.visible = false
+	ShowerNode.visible = true
+	WindowNode.visible = false
+	ShowerToWindowGif.visible = false
+
+func _on_LeftFlipBox_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_RightFlipBox_area_entered(area):
+	ShowerToWindowGif.visible = true
+	ShowerToWindowGif.frame = 0
+	ShowerToWindowGif.play
