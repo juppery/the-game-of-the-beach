a78a44ddfd1892d05a6154d2f6aabcbbd8909631
juppery <josiahbirt.13@gmail.com>
2023-08-16 19:45:13 -0400

    made it so the right flipbox sdisappears when looking at a gif and some otherthings

--- scripts/FlipViewScript.gd
+++ scripts/FlipViewScript.gd
@@ -2,12 +2,21 @@ extends Node2D
 onready var DoorNode = get_node("Door")
 onready var ShowerNode = get_node("Shower")
 onready var WindowNode = get_node("Window")
+onready var RightFlipBox = get_node("ViewControl/RightFlipBox")
 onready var GifNode = get_node("ViewControl/Gifs")
 onready var ShowerToWindowGif = get_node("ViewControl/Gifs/showertowindow")
 
 #just a check. this can be shower, window, door, or in between any of them. smart, huh?
 onready var lookingat = "shower"
 
+
+
+func _process(delta):
+	if lookingat == "door" or lookingat == "window" or lookingat == "shower":
+		RightFlipBox.visible = true
+	else:
+		RightFlipBox.visible = false
+		
 
 func _ready():
 	DoorNode.visible = false
@@ -20,7 +29,7 @@ func _on_LeftFlipBox_mouse_entered():
 
 
 func _on_RightFlipBox_mouse_entered():
-	print("big piner")
+	lookingat = "transition"
 	ShowerToWindowGif.frame = 0
 	ShowerToWindowGif.visible = true
 	ShowerToWindowGif.play()
@@ -28,6 +37,7 @@ func _on_RightFlipBox_mouse_entered():
 
 
 func _on_showertowindow_animation_finished():
+	lookingat = "window"
 	ShowerNode.visible = false
 	WindowNode.visible = true
 	ShowerToWindowGif.visible = false
