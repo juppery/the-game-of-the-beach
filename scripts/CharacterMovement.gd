abc4b2be138af1ab4714c54641f5563bdffc2804
juppery <josiahbirt.13@gmail.com>
2023-08-23 18:45:08 -0400

    put camera signals into the script attatched to Night1

--- scripts/CharacterMovement.gd
+++ scripts/CharacterMovement.gd
@@ -114,5 +114,99 @@ func _on_DannyTimer_timeout():
 #				dannyLocation = "kitchen" #we can change this
 	print("danny loc: "+dannyLocation)
 
+####CAMERA CONTROLLING-----------------------------------------------------------
 
 
+onready var CameraImages = get_node("Camera Images")
+#singular rooms
+onready var SchoolroomImages = get_node("Camera Images/Schoolroom")
+onready var BackroomImages = get_node("Camera Images/Backroom")
+onready var SamsRoomImages = get_node("Camera Images/Sams Room")
+onready var KitchenImages = get_node("Camera Images/Kitchen")
+onready var ParentsRoomImages = get_node("Camera Images/Parents Room")
+onready var PatioImages = get_node("Camera Images/Patio")
+onready var DownstairsImages = get_node("Camera Images/To Downstairs")
+onready var StairsImages = get_node("Camera Images/Stairs")
+onready var BensRoomImages = get_node("Camera Images/Bens Room")
+onready var DannysRoomImages = get_node("Camera Images/Dannys Room")
+onready var OutsideImages = get_node("Camera Images/OutsideWindow")
+#these ones just needs to be made
+onready var CamerasRootNode = get_parent()
+onready var buttonDown = CamerasRootNode.get_node("CameraButtons/ButtonSchoolroom")
+
+
+
+
+func _ready():
+	for child in CameraImages.get_children():
+		child.visible = false
+	
+	SchoolroomImages.visible = true
+	#buttonDown.pressed = true
+
+
+func _on_ButtonOutsideWindow_button_down():
+	for child in CameraImages.get_children():
+		child.visible = false
+	
+	OutsideImages.visible = true
+
+
+func _on_ButtonBensRoom_button_down():
+	for child in CameraImages.get_children():
+		child.visible = false
+	
+	BensRoomImages.visible = true
+
+
+func _on_ButtonDannysRoom_button_down():
+	for child in CameraImages.get_children():
+		child.visible = false
+	
+	DannysRoomImages.visible = true
+
+
+func _on_ButtonStairs_button_down():
+	for child in CameraImages.get_children():
+		child.visible = false
+	
+	StairsImages.visible = true
+
+
+func _on_ButtonSchoolroom_button_down():
+	for child in CameraImages.get_children():
+		child.visible = false
+	
+	SchoolroomImages.visible = true
+
+
+func _on_ButtonSamsRoom_button_down():
+	for child in CameraImages.get_children():
+		child.visible = false
+	
+	SamsRoomImages.visible = true
+
+
+func _on_ButtonKitchen_button_down():
+	for child in CameraImages.get_children():
+		child.visible = false
+	
+	KitchenImages.visible = true
+
+
+func _on_ButtonBackroom_button_down():
+	for child in CameraImages.get_children():
+		child.visible = false
+	
+	BackroomImages.visible = true
+
+
+
+
+
+func _on_ButtonParentsBedroom_button_down():
+	pass # Replace with function body.
+
+
+func _on_ButtonPatio_button_down():
+	pass # Replace with function body.
